584361ac18faea56d12748eb6d5bd0f2a1f1d8e5
BHAY-3DiTex <bhay@3ditex.com>
2023-12-19 15:51:16 +0100

    Modif nodeParameters Service

--- pyluos/services/nodeParameters.py
+++ pyluos/services/nodeParameters.py
@@ -4,39 +4,35 @@ class NodeParameters(Service):
 
     def __init__(self, id, alias, device):
         Service.__init__(self, 'NodeParameters', id, alias, device)
-        self._max_motor_nb = None
-        self._id_ring = None
-        self._id_disk = None
+        self._parameters = [None,None,None]
+        self._alias = alias 
 
     def receiveParameters(self):
         self._push_value('send_param', True)
 
+    def stopReceiveParameters(self):
+        self._push_value('send_param', False)
+
     @property
     def max_motor_nb(self):
         """ Maximum Motor Number. """
-        return self._max_motor_nb
-
-    @max_motor_nb.setter
-    def max_motor_nb(self, new_val):
-        self._max_motor_nb = new_val
-        self._push_value('max_motor_nb', new_val)
+        return self._parameters[0]
 
     @property
     def id_ring(self):
         """ Ring ID to know on wich ring the node is. """
-        return self._id_ring
+        return self._parameters[1]
 
     @property
     def id_disk(self):
         """ Disk ID to know on wich disk the node is. """
-        return self._id_disk
+        return self._parameters[2]
 
     def _update(self, new_state):
         Service._update(self, new_state)
-        if 'max_motor_nb' in new_state:
-            print('new_state = ',new_state)
-            self._max_motor_nb = new_state['max_motor_nb']
-        if 'id_ring' in new_state:
-            self._id_ring = new_state['id_ring']
-        if 'id_disk' in new_state:
-            self._id_disk = new_state['id_disk']
+        # print(self._alias)
+        # print('new_state = ',new_state)
+        if 'node_parameters' in new_state:
+            self._parameters = new_state['node_parameters']
+        
+        
